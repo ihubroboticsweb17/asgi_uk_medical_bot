3b8c63cbd596f89fa6a42d2d3b3e5eab5832d183
ashwinsujithh <ashwinsujith556@gmail.com>
2025-08-08 18:06:36 +0530

    role error fix

--- bed_data/serializer.py
+++ bed_data/serializer.py
@@ -29,14 +29,3 @@ class PatientRoomBedUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ['room_id', 'bed_id']
-
-    # def validate(self, data):
-    #     room = data.get('room', None)
-    #     bed = data.get('bed', None)
-        
-    #     # No restriction — both can be null, one, or both
-    #     # But if you want at least one, add:
-    #     # if not room and not bed:
-    #     #     raise serializers.ValidationError("Either room or bed must be provided.")
-
-    #     return data
